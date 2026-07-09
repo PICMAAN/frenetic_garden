e2b6fec6386994236d19b2a02467e78b5e949eeb
Chunksss <emmanuel.flores@uniat.edu.mx>
2026-07-09 16:58:00 -0600

    Sistema de Movimiento creado

--- Assets/Scripts/ControladorJugador.cs
+++ Assets/Scripts/ControladorJugador.cs
@@ -1,16 +1,84 @@
+using System;
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ControladorJugador : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //VARIABLES DE MOVIMIENTO
+    [SerializeField] private float speed;
+    private float moveX;
+    private float moveY;
+
+    //VARIABLES TIPO ESTRUCTURAS
+    Rigidbody2D rb2D;
+    //Animator animator;
+    public InputSystem_Actions acciones;
+    private SpriteRenderer spriteRenderer;
+
+
+    private void Awake()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+        acciones = new InputSystem_Actions();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnEnable()
     {
-        
+        acciones.Player.Enable();
+
+        acciones.Player.Move.performed += movimientoX;
+        acciones.Player.Move.canceled += movimientoX;
+
+        acciones.Player.Move.performed += movimientoY;
+        acciones.Player.Move.canceled += movimientoY;
+    }
+
+    void OnDisable()
+    {
+        acciones.Player.Move.performed -= movimientoX;
+        acciones.Player.Move.canceled -= movimientoX;
+
+        acciones.Player.Move.performed -= movimientoY;
+        acciones.Player.Move.canceled -= movimientoY;
+
+        acciones.Player.Disable();
+    }
+
+
+    void movimientoX(InputAction.CallbackContext ctx)
+    {
+        moveX = ctx.ReadValue<Vector2>().x;
+    }
+
+    void movimientoY(InputAction.CallbackContext ctx)
+    {
+        moveY = ctx.ReadValue<Vector2>().y;
+    }
+
+    void flip()
+    {
+        if (rb2D.linearVelocity.x > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (rb2D.linearVelocity.x < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.linearVelocityX = moveX * speed;
+        //animator.SetFloat("Vx", Mathf.Abs(rb2D.linearVelocityX));
+        flip();
+        rb2D.linearVelocityY = moveY * speed;
+        //animator.SetFloat("Vy", rb2D.linearVelocityY);
+        flip();
+
     }
 }
