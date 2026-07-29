ae995db19ab03e3d7ad4b17f9f3180a78f4e9982
Pulp0Seri0 <emmanuel.flores@uniat.edu.mx>
2026-07-29 00:22:38 -0600

    Slider de Volumen General CREADOI
    Dentro de este commit existe tanto el script como el slider que reaccionan al volumen general del juego.
    
    COSAS POR HACER:
    - Realizar un mixer para sonidos
    - Realizar un mixer para Musica
    
    Todos estos por separado.

---
+++ Assets/Scripts/Sonido/VolumenControl.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumenControl : MonoBehaviour
+{
+    [SerializeField] private AudioMixer Mixer;
+    [SerializeField] private Slider SliderVolumen;
+
+    private void Start()
+    {
+        // Cargar el valor actual del slider al iniciar
+        if (SliderVolumen != null)
+        {
+            SliderVolumen.onValueChanged.AddListener(SetMusicVolume);
+        }
+    }
+
+    public void SetMusicVolume(float sliderValue)
+    {
+        // Evitamos Log10(0) que da error; si el slider está en 0, silenciamos del todo (-80 dB)
+        if (sliderValue <= 0.0001f)
+        {
+            Mixer.SetFloat("DirectorVolumen", -80f);
+        }
+        else
+        {
+            // Convierte el rango [0.0001, 1] a decibelios [-80dB, 0dB]
+            float dbValue = Mathf.Log10(sliderValue) * 20f;
+            Mixer.SetFloat("DirectorVolumen", dbValue);
+        }
+    }
+}
